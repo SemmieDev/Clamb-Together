b7e23aeba9d7965756d8b45c2885950307209b63
Semmieboy YT <semvdmeij124@gmail.com>
2024-12-05 10:45:10 +0100

    Changed range of Y component in position data for the update packet

--- Clamb-Together/UpdatePacketData.cs
+++ Clamb-Together/UpdatePacketData.cs
@@ -9,6 +9,8 @@ public static class UpdatePacketData {
 
     private const uint POSITION_COMPONENT_SIZE = sizeof(short);
     private const uint ROTATION_COMPONENT_SIZE = sizeof(byte);
+    private const ushort POSITION_SCALE = 100;
+    private const ushort POSITION_Y_MIN = 10;
     private const float DEGREE_TO_BYTE = 255f / 360f;
     private const float BYTE_TO_DEGREE = 360f / 255f;
 
@@ -24,9 +26,9 @@ public static class UpdatePacketData {
 
     public static void WriteTransform(ref int offset, Transform transform) {
         var position = transform.position;
-        Marshal.WriteInt16(updatePacketData, offset, unchecked((short) (position.x * 100)));
-        Marshal.WriteInt16(updatePacketData, offset += sizeof(short), unchecked((short) (position.y * 100)));
-        Marshal.WriteInt16(updatePacketData, offset += sizeof(short), unchecked((short) (position.z * 100)));
+        Marshal.WriteInt16(updatePacketData, offset, unchecked((short) (position.x * POSITION_SCALE)));
+        Marshal.WriteInt16(updatePacketData, offset += sizeof(short), unchecked((short) (ushort) ((position.y + POSITION_Y_MIN) * POSITION_SCALE)));
+        Marshal.WriteInt16(updatePacketData, offset += sizeof(short), unchecked((short) (position.z * POSITION_SCALE)));
 
         var eulerAngles = transform.rotation.eulerAngles;
         Marshal.WriteByte(updatePacketData, offset += sizeof(short), unchecked((byte) (eulerAngles.x * DEGREE_TO_BYTE)));
@@ -37,9 +39,9 @@ public static class UpdatePacketData {
 
     public static void ReadTransform(ref int offset, out Vector3 position, out Quaternion rotation) {
         position = new Vector3(
-            Marshal.ReadInt16(updatePacketData, offset) / 100f,
-            Marshal.ReadInt16(updatePacketData, offset += sizeof(short)) / 100f,
-            Marshal.ReadInt16(updatePacketData, offset += sizeof(short)) / 100f
+            Marshal.ReadInt16(updatePacketData, offset) / (float) POSITION_SCALE,
+            ((ushort) Marshal.ReadInt16(updatePacketData, offset += sizeof(short)) - POSITION_Y_MIN * POSITION_SCALE) / (float) POSITION_SCALE,
+            Marshal.ReadInt16(updatePacketData, offset += sizeof(short)) / (float) POSITION_SCALE
         );
 
         rotation = Quaternion.Euler(
